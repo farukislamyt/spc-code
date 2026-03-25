a93f10e11ad773ecff73f2889f8f295c96f6323c
farukislamyt <farukislamyt@gmail.com>
2026-03-25 22:01:10 +0600

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+import { generateSPC } from "./encoder.js";
+import { decodeSPC } from "./decoder.js";
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+document.getElementById("generateBtn").onclick = () => {
+  const text = document.getElementById("textInput").value;
+  generateSPC(text, ctx);
+};
+
+document.getElementById("decodeBtn").onclick = () => {
+  const text = decodeSPC(canvas);
+  document.getElementById("output").innerText = text;
+};
